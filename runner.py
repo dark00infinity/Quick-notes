c4a5649aa9cb7d2a6764922f9c2abce435e334f8
Dark00infinity <47387653+dark00infinity@users.noreply.github.com>
2021-04-14 15:28:22 +0530

    Add files via upload

---
+++ runner.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  4 13:00:12 2021
+
+@author: Asus
+"""
+import keyboard,os
+while True:
+    if keyboard.is_pressed('w'):
+        os.system("python Snipping.py")
+    elif keyboard.is_pressed('a'):
+        break
